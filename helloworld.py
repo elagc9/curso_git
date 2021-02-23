2bdc32070742acff1cd391b6644ca8bf571c5a2f
elagc9 <antonio.gonzalez-cuellar.taboada@alumnos.upm.es>
2021-02-23 17:42:49 +0100

    modified:   helloworld.py 	new file:   printfruits.py

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,3 @@
-print("Hello World!")
+print("Hello World!\n")
+print("Next example: add new code:\n")
+print("New code added\n")
---
+++ printfruits.py
@@ -0,0 +1,4 @@
+fruits = ["apple", "banana", "orange", "cherry", "watermelon", "pineapple"]
+for x in fruits:
+	print(x + "\n")
+	
