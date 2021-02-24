9b84e487db9329ff99486f601305715ce7da0911
Sergio-VR <74207251+Sergio-VR@users.noreply.github.com>
2021-02-24 17:33:06 +0100

    Pares e impares

--- helloworld.py
+++ helloworld.py
@@ -3,3 +3,11 @@ print("Next example: add new code:\n")
 print("New code added\n")
 Fallo eliminado. Good job man!
 
+
+print 'pares e inpares'
+num=int(raw_input('introduce algun numero entero'))
+
+if num%2 == 0
+print 'este numero es par'
+if num%2 != 0
+print 'este numero es inpar
